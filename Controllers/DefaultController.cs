90ee8f1aec00d1c13f09181d19ea8a032b59e6af
İbrahim TULUMCU <94108626+ibrahimTlmcu@users.noreply.github.com>
2023-10-26 19:34:11 +0300

    #

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SeyahatProje.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
