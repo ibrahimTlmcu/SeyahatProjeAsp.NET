f78036f4d496b0a4f0e458ff5d732cda68badfa2
İbrahim TULUMCU <94108626+ibrahimTlmcu@users.noreply.github.com>
2023-12-03 19:38:25 +0300

    #

---
+++ ViewComponents/Default/_SliderPartail.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace SeyahatProje.ViewComponents.Default
+{
+    public class _SliderPartail : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View(); 
+        }
+    }
+}
