18c446f3968e0b73d273318bf1dc0fbbb8c600a1
İbrahim TULUMCU <94108626+ibrahimTlmcu@users.noreply.github.com>
2023-12-07 01:38:32 +0300

    dKatmanlar düzenlendi

---
+++ ViewComponents/Default/_Feature.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concrete;
+using DataAccesLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeyahatProje.ViewComponents.Default
+{
+    public class _Feature :ViewComponent
+    {
+        FeatureManager featureManager = new FeatureManager(new EfFatureDal());
+
+        public IViewComponentResult Invoke()
+        {
+            
+            
+            ViewBag.v1 = featureManager.TGetList();
+            return View();
+        }
+
+    }
+}
---
+++ ViewComponents/Default/_Static.cs
@@ -0,0 +1,22 @@
+﻿using DataAccesLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+
+
+namespace SeyahatProje.ViewComponents.Default
+{
+    public class _Static :ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            
+            using var c = new Context();
+            ViewBag.v1 = c.Destinations.Count();
+            ViewBag.v2 = c.Guides.Count();
+            ViewBag.v3 = "a123";
+            return View();
+        }
+    }
+}
